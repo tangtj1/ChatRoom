3f4a99642642c3170128c36f8909efa3a51b76ef
tang <762550778@qq.com>
2018-05-24 22:50:38 +0800

    [ADD] 加入shiro授权验证Realm类

---
+++ src/main/java/cn/tangtj/chatroom/security/SystemAuthorizingRealm.java
@@ -0,0 +1,46 @@
+package cn.tangtj.chatroom.security;
+
+import cn.tangtj.chatroom.entity.User;
+import cn.tangtj.chatroom.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * 系统登录及其权限处理
+ *
+ * @author tang
+ */
+@Slf4j
+@Component
+public class SystemAuthorizingRealm extends AuthorizingRealm {
+
+    private final UserService userService;
+
+    @Autowired
+    public SystemAuthorizingRealm(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        String username = (String) token.getPrincipal();
+        String password = new String((char[]) token.getCredentials());
+        log.info("用户尝试登录:{}",username);
+        User user = userService.findByUsername(username);
+        if (user == null){
+            throw new UnknownAccountException();
+        }
+        return new SimpleAuthenticationInfo(user,password,getName());
+    }
+}
