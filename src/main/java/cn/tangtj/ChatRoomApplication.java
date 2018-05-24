2a2002be8f6bf2b8e1d7b2c12c96296f1f90b351
tang <762550778@qq.com>
2018-05-24 22:56:33 +0800

    [MOD] 移动类包

--- src/main/java/cn/tangtj/ChatRoomApplication.java
+++ src/main/java/cn/tangtj/ChatRoomApplication.java
@@ -1,4 +1,4 @@
-package cn.tangtj.chatroom;
+package cn.tangtj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/cn/tangtj/ServletInitializer.java
+++ src/main/java/cn/tangtj/ServletInitializer.java
@@ -1,4 +1,4 @@
-package cn.tangtj.chatroom;
+package cn.tangtj;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
