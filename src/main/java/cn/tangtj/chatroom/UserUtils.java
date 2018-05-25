fbbde93fcc0f83889a617a81b3e9bb28e0a258b1
tang <762550778@qq.com>
2018-05-25 17:57:50 +0800

    [ADD] 添加user通用方法

---
+++ src/main/java/cn/tangtj/chatroom/UserUtils.java
@@ -0,0 +1,23 @@
+package cn.tangtj.chatroom;
+
+import cn.tangtj.chatroom.entity.User;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+
+/**
+ * @author tang
+ */
+public class UserUtils {
+
+    public static User getPrincipal(){
+        Subject subject = getSubject();
+        if (subject.isAuthenticated()){
+            return (User) subject.getPrincipal();
+        }
+        return null;
+    }
+
+    public static Subject getSubject(){
+        return SecurityUtils.getSubject();
+    }
+}
