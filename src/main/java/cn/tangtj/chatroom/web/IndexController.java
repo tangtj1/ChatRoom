c4b3408969a595f6011ff134ee5eafbfb0694890
tang <762550778@qq.com>
2018-05-25 18:10:22 +0800

    [ADD] 添加首页控制类

---
+++ src/main/java/cn/tangtj/chatroom/web/IndexController.java
@@ -0,0 +1,22 @@
+package cn.tangtj.chatroom.web;
+
+import cn.tangtj.chatroom.UserUtils;
+import cn.tangtj.chatroom.entity.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * 访问首页跳转到登录界面
+ *
+ * @author tang
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/")
+    public String index(){
+        User user = UserUtils.getPrincipal();
+        return "redirect:/login";
+    }
+}
