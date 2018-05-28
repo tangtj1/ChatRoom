b95967e239d634ccb064b1c31a8f9427e250d721
tang <762550778@qq.com>
2018-05-28 19:19:38 +0800

    [ADD] 配置WebSocket拦截器

--- src/main/java/cn/tangtj/chatroom/websocket/WebSocketInterceptor.java
+++ src/main/java/cn/tangtj/chatroom/websocket/WebSocketInterceptor.java
@@ -1,16 +1,28 @@
 package cn.tangtj.chatroom.websocket;
 
+import cn.tangtj.chatroom.UserUtils;
+import cn.tangtj.chatroom.entity.User;
+import cn.tangtj.chatroom.websocket.util.WebSocketHandlerKey;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 import java.util.Map;
 
+/**
+ *
+ * WebSocket 拦截器配置
+ *
+ * @author tang
+ */
 public class WebSocketInterceptor extends HttpSessionHandshakeInterceptor {
 
     /**
-     * todo:没写
+     *
+     * 判断是否有Group_key,和是否登录，否则进行拦截
+     *
      * @param request
      * @param response
      * @param wsHandler
@@ -20,6 +32,11 @@ public class WebSocketInterceptor extends HttpSessionHandshakeInterceptor {
      */
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        String groupKey = ((ServletServerHttpRequest) request).getServletRequest().getParameter(WebSocketHandlerKey.GROUP_KEY);
+        User user = UserUtils.getPrincipal();
+        if (user == null || groupKey == null || "".equals(groupKey)){
+            return false;
+        }
         return super.beforeHandshake(request, response, wsHandler, attributes);
     }
 }
