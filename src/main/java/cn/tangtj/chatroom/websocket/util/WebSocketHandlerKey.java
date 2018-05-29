231f5b6607d5420569495e9653604d340f8c7168
tang <762550778@qq.com>
2018-05-29 23:24:49 +0800

    [ADD] 添加静态字段类

---
+++ src/main/java/cn/tangtj/chatroom/websocket/util/WebSocketHandlerKey.java
@@ -0,0 +1,10 @@
+package cn.tangtj.chatroom.websocket.util;
+
+/**
+ * @author tang
+ */
+public final class WebSocketHandlerKey {
+    public static final String GROUP_KEY = "Group_Key";
+    public static final String USER_KEY = "User_Key";
+    public static final String INFO_KEY = "Info_Key";
+}
