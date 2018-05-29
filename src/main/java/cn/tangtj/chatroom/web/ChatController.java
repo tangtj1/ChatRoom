c42dffe0bf685ff980d4f8510781556e710df397
tang <762550778@qq.com>
2018-05-29 23:25:13 +0800

    [ADD] 加入数个url对应的控制方法

--- src/main/java/cn/tangtj/chatroom/web/ChatController.java
+++ src/main/java/cn/tangtj/chatroom/web/ChatController.java
@@ -1,7 +1,16 @@
 package cn.tangtj.chatroom.web;
 
+import cn.tangtj.chatroom.AjaxBodyMap;
+import cn.tangtj.chatroom.entity.Group;
+import cn.tangtj.chatroom.service.ChatRoomService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * @author tang
@@ -10,8 +19,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/chat")
 public class ChatController {
 
+    private final ChatRoomService chatRoomService;
+
+    @Autowired
+    public ChatController(ChatRoomService chatRoomService) {
+        this.chatRoomService = chatRoomService;
+    }
+
     @RequestMapping()
-    public String chatIndex() {
+    public String chatIndex(Model model) {
+        List list = chatRoomService.getAllGroup();
+        long count = chatRoomService.getOnlineUserCount();
+        model.addAttribute("romeList",list);
+        model.addAttribute("onlineCount",count);
         return "chat";
+    }
+
+    @ResponseBody
+    @RequestMapping("/create")
+    public AjaxBodyMap createChatRome(String name) {
+        AjaxBodyMap body = new AjaxBodyMap();
+        Group group = chatRoomService.createGroup(name);
+        if (group != null) {
+            body.setOk(true);
+            body.addBody("msg","创建成功");
+            return body;
+        }
+        body.setOk(false);
+        body.addBody("msg","创建失败");
+        return body;
+    }
+
+    @ResponseBody
+    @RequestMapping("/search/{keyOrName}")
+    public List<Group> searchRome(@PathVariable("keyOrName") String keyOrName){
+        List<Group> list =  chatRoomService.searchGroup(keyOrName);
+        return list;
     }
 }
